129827c3033a02f9a0bccc1d7356dec5ac08db5d
gelee <gelee@edacom.co.kr>
2024-01-29 16:55:51 +0900

    Fixed: backend 폴더 이동

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeedModule } from './feed/feed.module';
+import { User } from './user/entities/user.entity';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -15,9 +17,10 @@ import { FeedModule } from './feed/feed.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
+    UserModule,
     FeedModule,
   ]
 })
