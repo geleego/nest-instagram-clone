80da58b59708434a1b5e48032c505d3b06213fb4
gelee <gelee@edacom.co.kr>
2024-01-29 16:19:52 +0900

    Feat: Nest와 DB 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { FeedModule } from './feed/feed.module';
 
 @Module({
-  imports: [FeedModule]
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.dev']
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    FeedModule,
+  ]
 })
 export class AppModule {}
